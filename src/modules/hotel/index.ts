aeffc7cfa726bf85a8f83c3f052a5cb1ac31e57b
fazliddinquvatboyev8@gmail.com <fazliddinquvatboyev8@gmail.com>
2024-12-16 20:06:40 +0500

    fix: issue

--- src/modules/hotel/index.ts
+++ src/modules/hotel/index.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { Hotel } from "./model/hotel.model";
+import hotelJoiSchema from "./validation";
 
 export const hotelRoute = Router();
 
 hotelRoute.post("/create", async (req, res): Promise<any> => {
-  // const { error, value } = hotelJoiSchema.validate(req.body);
-  // if (error) {
-  //   return res.status(400).send(`Error during creating hotel! ${error}`);
-  // }
+  const { error, value } = hotelJoiSchema.validate(req.body);
+  if (error) {
+    return res.status(400).send(`Error during creating hotel! ${error}`);
+  }
   const createdHotel = await Hotel.create(req.body);
 
   res.status(201).json(createdHotel);
