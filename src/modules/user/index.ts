81579797bed0af5718c9db7857d40a01cd39dc23
fazliddinquvatboyev8@gmail.com <fazliddinquvatboyev8@gmail.com>
2024-12-17 00:29:02 +0500

    add: auth

--- src/modules/user/index.ts
+++ src/modules/user/index.ts
@@ -1,6 +1,14 @@
-import { Router } from "express";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import express, { Router } from "express";
+import jwt from "jsonwebtoken";
 import { User } from "./model/user.model";
 import userJoiSchema from "./validation";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
 
 export const userRoute = Router();
 
@@ -18,11 +26,27 @@ userRoute.post("/signUp", async (req, res): Promise<any> => {
   const { error, value } = userJoiSchema.validate(req.body);
 
   if (error) {
+    console.log(error);
     return res.status(400).send("User is not created!");
   }
 
-  const createdUser = await User.create(value);
-  res.status(201).send(createdUser);
+  const hashedPassword = await bcrypt.hash(value.password, 8);
+
+  const newUser = new User({
+    ...req.body,
+    password: hashedPassword,
+  });
+  const createdUser = await newUser.save();
+  res.status(201).json(createdUser);
+});
+
+userRoute.post("/signIn", (req, res) => {
+  const userEmail = req.body.email;
+
+  const user = { email: userEmail };
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json(accessToken);
 });
 
 userRoute.put("/changeRole/:id", async (req, res) => {
@@ -34,3 +58,18 @@ userRoute.put("/changeRole/:id", async (req, res) => {
     res.status(404).send("User not found!");
   }
 });
+
+function authToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.status(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).send("Token is not valid!");
+    }
+    req.user = user;
+    next();
+  });
+}
